a33c8f9cea26e371785236d5932ba8400c38d299
Sebastian Spicker <sebastian.spicker@googlemail.com>
2023-03-03 17:44:00 +0100

    docs: capture the Linux LoLa interoperability goal and licensing boundary

---
+++ README.md
@@ -0,0 +1,12 @@
+# open-lola
+
+A working tree for open-lola with an evolving implementation history.
+
+## Overview
+open-lola keeps the active bootstrap push work visible.
+
+## Status
+Project phase: bootstrap.
+
+## Usage
+- Made the the main flow assumptions easier to check later.
---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+"""Package exports."""
+
+# current lane: capture_the_linux_lola_interoperability_goal_and_licensing_boundary
+def capture_the_linux_lola_interoperability_goal_and_licensing_boundary_task() -> dict[str, str]:
+    return {"scope": "capture the linux lola interoperability goal and licensing boundary", "status": "ready"}
